71bea90266172e0d88407a64e4b1826e24187436
belovdg <belovdg87@yandex.ru>
2020-12-08 22:14:09 +0300

    Initial commit

---
+++ src/Studying/Main.java
@@ -0,0 +1,12 @@
+package Studying;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 10;
+		if (a < b) {System.out.println("Минимальное " + a);}
+		if (a > b) {System.out.println("Минимальное " + b);}
+		if (a == b) {System.out.println("Минимального нет ");}
+	}
+}
